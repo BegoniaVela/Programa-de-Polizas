e9a91d71c31d6d5c5dbe78da1c95298295664e36
Begonia Vela <167794679+BegoniaVela@users.noreply.github.com>
2025-10-14 00:31:52 -0500

    Update Proyecto_Polizas.py

--- Proyecto_Polizas.py
+++ Proyecto_Polizas.py
@@ -207,22 +207,3 @@ def filtrar_por_semaforo_simple():
     else:
         print("No se encontraron pólizas con ese semáforo")
 
-# Mostrar y filtrar (pila/cola)
-def mostrar_polizas_pila(lista_polizas):
-    pila = deque(lista_polizas)
-    print("\n--- PÓLIZAS (PILA - Últimas a primero) ---")
-    while pila:
-        poliza = pila.pop()
-        print(poliza)
-
-def mostrar_polizas_cola(lista_polizas):
-    cola = deque(lista_polizas)
-    print("\n--- PÓLIZAS (COLA - Primeras a últimas) ---")
-    while cola:
-        poliza = cola.popleft()
-        print(poliza)
-
-    except Exception as e:
-        print(f"Error al agregar póliza: {e}")
-
-
